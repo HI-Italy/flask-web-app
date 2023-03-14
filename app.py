1acb97062cb3cee4a6f881047a8cc08e79ddadbc
arognoni <alessandro.rognoni@capgemini.com>
2023-03-14 15:45:26 +0100

    Ready for the api call

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from datetime import datetime
+from database import load_db, save_db, get_plot_schema, add_row
+import requests
 
+
+db = load_db("crypto_data.csv")
 app = Flask(__name__)
 
 
@@ -13,6 +17,27 @@ def index():
 def data():
     date = str(datetime.now())
     return render_template("data.html", date=date)
+
+@app.route("/crypto/<int:index>", methods=["GET", "POST"])
+def crypto(index):
+
+    if request.method == "POST":
+        pass
+
+    name = db.columns[index + 1]
+    price = db[name].iloc[-1]
+    schema = get_plot_schema(db, name)
+
+    return render_template(
+        "crypto.html",
+        name=name,
+        price=price,
+        schema=schema,
+        index=index
+    )
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
