ddd826753c9aeed46783c47b1fbf4d01613a1e86
arognoni <alessandro.rognoni@capgemini.com>
2024-02-20 08:46:53 +0100

    Change update mechanism

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 import requests
-from flask import Flask, abort, render_template, request
+from flask import Flask, abort, redirect, render_template, url_for
 
 from constants import COLUMN_MAP, URL
 from database import add_row, get_plot_schema, load_db, save_db
@@ -52,19 +52,13 @@ def about_us():
     return render_template("about_us.html")
 
 
-@app.route("/crypto/<int:index>", methods=["GET", "POST"])
-def crypto(index):
-    """Cryptocurrency pages.
-
-    To distinguish between cryptos, we use the index
-    variable:
-
-    /crypto/0 -> Bitcoin
-    /crypto/1 -> Dogecoin
-    /crypto/2 -> Ethereum
+@app.route("/update/<int:index>")
+def update(index):
+    """Update the cryptocurrency prices.
 
     The POST method is used to update the price using
     the Binance API. The data we get has this format:
+
     [
       {
           "symbol":"BTCUSDT",
@@ -80,6 +74,47 @@ def crypto(index):
       }
     ]
 
+    The user is then redirected to the original page.
+
+    Args:
+        index (int): Index of the cryptocurrency.
+    """
+    # Get the JSON prices of all cryptos through the Binance API.
+    results = requests.get(URL)
+    results = results.json()
+
+    # After this stage we get:
+    # {"BTCUSDT": "1", "DOGEUSDT": "2", "ETHUSDT": "3"}
+    unformatted_row = {result["symbol"]: result["price"] for result in results}
+
+    # Now it will have the shape of a row:
+    # {"Bitcoin": 1.0, "Dogecoin": 2.0, "Ethereum": 3.0}
+    new_row = {
+        COLUMN_MAP[symbol]: float(price)
+        for symbol, price in unformatted_row.items()
+    }
+
+    # Add date and time to the new row
+    new_row["Datetime"] = str(datetime.now())
+
+    # Add the row and save the db as a csv file
+    add_row(db, new_row)
+    save_db(db, "crypto_data.csv")
+
+    return redirect(url_for("crypto", index=index))
+
+
+@app.route("/crypto/<int:index>")
+def crypto(index):
+    """Cryptocurrency pages.
+
+    To distinguish between cryptos, we use the index
+    variable:
+
+    /crypto/0 -> Bitcoin
+    /crypto/1 -> Dogecoin
+    /crypto/2 -> Ethereum
+
     Args:
         index (int): Index of the cryptocurrency.
     """
@@ -90,33 +125,6 @@ def crypto(index):
     # These pages don't exist
     if index > max_index:
         abort(404)
-
-    # Update all prices
-    if request.method == "POST":
-
-        # Get the JSON prices of all cryptos through the Binance API.
-        results = requests.get(URL)
-        results = results.json()
-
-        # After this stage we get:
-        # {"BTCUSDT": "1", "DOGEUSDT": "2", "ETHUSDT": "3"}
-        unformatted_row = {
-            result["symbol"]: result["price"] for result in results
-        }
-
-        # Now it will have the shape of a row:
-        # {"Bitcoin": 1.0, "Dogecoin": 2.0, "Ethereum": 3.0}
-        new_row = {
-            COLUMN_MAP[symbol]: float(price)
-            for symbol, price in unformatted_row.items()
-        }
-
-        # Add date and time to the new row
-        new_row["Datetime"] = str(datetime.now())
-
-        # Add the row and save the db as a csv file
-        add_row(db, new_row)
-        save_db(db, "crypto_data.csv")
 
     # Name of the crypto specified by the index
     name = db.columns[index + 1]
