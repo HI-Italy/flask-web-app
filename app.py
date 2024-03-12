00a1ae99bf509306fa00ca1b6baa1cfb019f6386
Elisa_Rasca <elisa.rasca2@capgemini.com>
2024-03-12 13:00:06 +0100

    prima versione completa di index.html

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from datetime import datetime
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello world!"
+    return render_template("index.html")
+
+@app.route("/today")
+def today():
+    return f"{datetime.now()}"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
